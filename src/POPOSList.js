1d226b3b9099d962200886e075bfa8b3f5e87aa7
jcoyle77 <60405340+jcoyle77@users.noreply.github.com>
2020-04-30 11:43:01 -0700

    updated changes

---
+++ src/POPOSList.js
@@ -0,0 +1,17 @@
+// src/POPOSList.js
+
+import POPOSSpace from './POPOSSpace';
+import React from 'react';
+
+function POPOSList() {
+  return (
+    <div>
+      <POPOSSpace />
+      <POPOSSpace />
+      <POPOSSpace />
+      <POPOSSpace />
+    </div>
+  )
+}
+
+export default POPOSList
---
+++ src/POPOSSpace.js
@@ -0,0 +1,15 @@
+// src/Project.js
+
+import React from 'react'
+
+function POPOSSpace() {
+  return (
+    <div>
+      <img src="/images/100-pine.jpg" width="300" height="300" alt="Hello" />
+      <h1>Name...</h1>
+      <div>Address...</div>
+    </div>
+  )
+}
+
+export default POPOSSpace
